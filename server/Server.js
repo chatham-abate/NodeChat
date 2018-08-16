df3a6ccd9421afd88f3073a0f41427ebf5ca1cfa
Chatham Abate <chathamabate@gmail.com>
2018-08-16 11:01:25 -0400

    Command Line Changes.

--- server/Server.js
+++ server/Server.js
@@ -190,7 +190,8 @@ app.post("/api/promoteUser", (req, res) => {
 console.log("--------------------");
 console.log("Welcome to NodeChat!");
 console.log("-------------------- \n");
-console.log("Use Command help for list of Commands.\n")
+console.log("Use Command help for list of Commands.\n");
+console.log("*** If you are not loading server data froma save, you must use command init. ***");
 
 // Command Line Controller for the Server.
 rl.on('line', (cmd) => {
